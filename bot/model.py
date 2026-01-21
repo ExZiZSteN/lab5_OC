b3320824ab2d61b22604e4d0ce7e7d3e8e0ac4a3
ars <lrsck@mail.ru>
2026-01-21 23:49:35 +0500

    Add new code

---
+++ bot/model.py
@@ -0,0 +1,42 @@
+import os
+import psycopg2
+from Nodes import *
+
+
+class Database:
+    def __init__(self):
+        """
+        Подключение к БД
+        Args:
+            db_url = URL базы данных
+        """
+        db_url = os.getenv('DATABASE_URL')
+
+        # подключение к postgresql
+        self.connection = psycopg2.connect(db_url)
+        self.cursor = self.connection.cursor()
+
+        self.create_tables()
+
+    def create_tables(self):
+        """
+        Создание таблиц в БД
+        """
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS users(
+                id SERIAL PRIMARY KEY,
+                telegram_id BIGINT UNIQUE NOT NULL,
+                username VARCHAR(255)
+            )
+                            """)
+        
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS notes(
+                id SERIAL PRIMARY KEY,
+                user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+                title VARCHAR(255) NOT NULL,
+                content TEXT NOT NULL
+            )
+                            """)
+        
+        self.connection.commit()
